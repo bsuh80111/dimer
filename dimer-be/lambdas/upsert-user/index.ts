dfe909eeda20fcebdd5de78d6fdc18480acd108d
Brian Suh <bsuh@ncsu.edu>
2023-11-17 20:28:21 -0500

    Upsert lambda setup w/ API Gateway

--- dimer-be/lambdas/upsert-user/index.ts
+++ dimer-be/lambdas/upsert-user/index.ts
@@ -1,38 +1,61 @@
-import { Handler } from "aws-lambda";
+import { APIGatewayProxyEvent, Handler, APIGatewayProxyResultV2 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, UpdateCommand, UpdateCommandInput } from "@aws-sdk/lib-dynamodb";
+import { randomUUID } from "crypto";
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
-export const handler: Handler = async (event, context) => {
-    console.log('UPSERT USER');
+export const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResultV2> = async (event: APIGatewayProxyEvent, context) => {
+    console.log('Event: ' + event);
 
+    // Error if no body/data is provided
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'No data provided'
+            })
+        }
+    }
+
+    const data = JSON.parse(event.body); // Parse data
+    const userId = data.id ?? randomUUID(); // Generate new ID if none provided
+
+    // Set update timestamp
     let updateExpression = "SET lastUpdate = :updateTime";
     let updateAttributes: Record<string, any> = {
         ':updateTime': new Date().toISOString()
     };
 
-    for (let key in event.data) {
+    // Iterate through data keys to create update expression
+    // Allows us to upsert data
+    // Idea from https://dev.to/dengel29/flexible-upsert-with-dynamodb-397j
+    for (let key in data) {
         if (key !== 'id') {
             updateExpression += `, ${key} = :${key}`;
-            updateAttributes[`:${key}`] = event.data[key];
+            updateAttributes[`:${key}`] = data[key];
         }
     }
 
-
+    // Initialize update parameters
     const updateItemParams: UpdateCommandInput = {
         TableName: 'DimerUsers',
-        Key: {
-            id: event.data.id
-        },
+        Key: { id: userId }, // Primary Key
         UpdateExpression: updateExpression,
         ExpressionAttributeValues: updateAttributes,
-        ReturnValues: "ALL_NEW"
+        ReturnValues: "ALL_NEW" // Returns all properties of the modified/inserted item
     };
-
     console.log(updateItemParams);
+
+    // Execute update
     const response = await docClient.send(new UpdateCommand(updateItemParams));
-    console.log('RESPONSE: ' + response);
-    return response;
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: data.id ? `Updated user: ${userId}` : `Inserted user: ${userId}`,
+            result: response.Attributes
+        })
+    };
 };
--- dimer-be/main.tf
+++ dimer-be/main.tf
@@ -90,19 +90,69 @@ resource "aws_dynamodb_table" "dimer_users" {
 }
 
 
+### API Gateway ###
+resource "aws_apigatewayv2_api" "dimer" {
+  name          = "DimerApi"
+  protocol_type = "HTTP"
+  description   = "Request handler for Dimer BE"
+}
+
+resource "aws_apigatewayv2_route" "post_handler" {
+  api_id    = aws_apigatewayv2_api.dimer.id
+  route_key = "POST /user"
+
+  target = "integrations/${aws_apigatewayv2_integration.upsert_user_lambda.id}"
+}
+
+resource "aws_apigatewayv2_integration" "upsert_user_lambda" {
+  api_id = aws_apigatewayv2_api.dimer.id
+
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.upsert_user.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_stage" "test" {
+  api_id      = aws_apigatewayv2_api.dimer.id
+  name        = "test"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_deployment" "dimer" {
+  api_id      = aws_apigatewayv2_api.dimer.id
+  description = "Dimer API deployment"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
 ### Lambdas ###
 
 data "archive_file" "upsert-user-zip" {
- source_file = "dist/upsert-user/index.js"
- output_path = "dist/upsert-user/upsert-user.zip"
- type = "zip"
+  source_file = "dist/upsert-user/index.js"
+  output_path = "dist/upsert-user/upsert-user.zip"
+  type        = "zip"
 }
 
 resource "aws_lambda_function" "upsert_user" {
-  function_name = "dimer-upsert-user"
-  role = aws_iam_role.dimer_lambda.arn
-  runtime = "nodejs20.x"
-  handler = "lambdas/upsert-user/index.handler"
-  filename = data.archive_file.upsert-user-zip.output_path
+  function_name    = "dimer-upsert-user"
+  role             = aws_iam_role.dimer_lambda.arn
+  runtime          = "nodejs20.x"
+  handler          = "lambdas/upsert-user/index.handler"
+  filename         = data.archive_file.upsert-user-zip.output_path
   source_code_hash = data.archive_file.upsert-user-zip.output_base64sha256
+}
+
+# Allow invokations from API Gateway
+resource "aws_lambda_permission" "api_gateway_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.upsert_user.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_apigatewayv2_api.dimer.execution_arn}/*/*"
 }
